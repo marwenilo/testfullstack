c4bb0c9ee25f1167fce6847e05bd6aeb93d39a21
mohamed hanafi hassanet <mohamedhanafihassanet@gmail.com>
2019-11-01 23:39:28 +0100

    one profile design

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,39 +1,76 @@
+// import React, { Fragment } from 'react';
+// import PropTypes from 'prop-types';
+// import './profile.css'
+
+// const ProfileAbout = ({
+//   profile: {
+//     bio, 
+//     countries,
+//     user: { name }
+//   }
+// }) => {
+//   return (
+//     <div className="profileAbout">
+//       { bio && (
+//       <Fragment>
+//         <h2 className="aboutTitle">{name.trim().split('')[0]}'s Bio</h2>
+//           <p>
+//             {bio}
+//           </p>
+//           <div className="line"></div>
+//       </Fragment>
+//       )}      
+//           <h2 className="aboutTitle">Visited Countries</h2>
+//           <div className="skills">
+//           {countries.map((country, index) =>(
+//         <div key={index} className="p-1" >
+//           <i className="fa fa-check" style={{color:"#037367"}}></i> {country}
+//           </div>
+//       ))}
+//           </div>
+//         </div>
+//   )
+// }
+ 
+// ProfileAbout.propTypes = {
+//   profile: PropTypes.object.isRequired
+
+// }
+
+// export default ProfileAbout
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-
+import './profile.css'
 const ProfileAbout = ({
-  profile: {
-    bio, 
-    countries,
-    user: { name }
-  }
+ profile: {
+   bio,
+   countries,
+   user: { name }
+ }
 }) => {
-  return (
-    <div className="profileAbout">
-      { bio && (
-      <Fragment>
-        <h2 className="aboutTitle">{name.trim().split('')[0]}'s Bio</h2>
-          <p>
-            {bio}
-          </p>
-          <div className="line"></div>
-      </Fragment>
-      )}      
-          <h2 className="aboutTitle">Visited Countries</h2>
-          <div className="skills">
-          {countries.map((country, index) =>(
-        <div key={index} className="p-1" >
-          <i className="fa fa-check" style={{color:"#037367"}}></i> {country}
-          </div>
-      ))}
-          </div>
-        </div>
-  )
+ return (
+   <div className="profileAbout">
+     { bio && (
+     <Fragment>
+       <h2 className="aboutTitle">{name.trim().split('')[0]}'s Bio</h2>
+         <p className="p-profil-about">
+           {bio}
+         </p>
+         <div className="line"></div>
+     </Fragment>
+     )}
+         <h2 className="aboutTitle">Visited Countries</h2>
+         <div className="skills">
+         {countries.map((country, index) =>(
+       <div key={index} className="p-1" >
+         <i className="fa fa-check" style={{color:"#037367"}}></i> {country}
+         </div>
+     ))}
+         </div>
+       </div>
+ )
 }
- 
 ProfileAbout.propTypes = {
-  profile: PropTypes.object.isRequired
-
+ profile: PropTypes.object.isRequired
 }
-
 export default ProfileAbout
--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,57 +1,126 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import './profile.css'
+
+// const ProfileTop = ({profile: {
+//   status,
+//   // company,
+//   location,
+//   social,
+//   user: { name, avatar }
+// }}) => {
+//   return (
+//     <div className="profileTop">
+//     <img
+//       className="round-img my-1"
+//       src={avatar}
+//       alt="avatar"
+//     />
+//     <h1 className="memberName">{name.toUpperCase()}</h1>
+//     <p className="memberJob"> {status}   </p>
+//     <p className="memberAdress">{location && <span>{location}</span>}</p>
+//     <div className="icons my-1">
+      
+//       {
+//         social && social.facebook &&  (
+//           <a href={social.facebook} target="_blank" rel="noopener noreferrer">
+//                   <i className="fab fa-facebook fa-2x"></i>
+//                 </a>
+//         )
+//       }
+         
+//          {
+//         social && social.youtube &&  (
+//           <a href={social.youtube} target="_blank" rel="noopener noreferrer">
+//         <i className="fab fa-youtube fa-2x"></i>
+//                 </a>
+//         )
+//       }
+         
+//          {
+//         social && social.instagram &&  (
+//           <a href={social.instagram} target="_blank" rel="noopener noreferrer">
+//         <i className="fab fa-instagram fa-2x"></i>
+//                 </a>
+//         )
+//       }
+      
+//     </div>
+//   </div>
+//   )
+// }
+
+// ProfileTop.propTypes = {
+//   profile: PropTypes.object.isRequired
+
+// }
+
+// export default ProfileTop
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const ProfileTop = ({profile: {
+import './profile.css'
+const ProfileTop = ({
+profile: {
   status,
   // company,
   location,
   social,
   user: { name, avatar }
-}}) => {
-  return (
-    <div className="profileTop">
-    <img
-      className="round-img my-1"
-      src={avatar}
-      alt="avatar"
-    />
-    <h1 className="memberName">{name.toUpperCase()}</h1>
-    <p className="memberJob"> {status}   </p>
-    <p className="memberAdress">{location && <span>{location}</span>}</p>
-    <div className="icons my-1">
-      
-      {
-        social && social.facebook &&  (
-          <a href={social.facebook} target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-facebook fa-2x"></i>
-                </a>
-        )
-      }
-         
-         {
-        social && social.youtube &&  (
-          <a href={social.youtube} target="_blank" rel="noopener noreferrer">
+}
+}) => {
+return (
+  <div class='container'>
+    <div class='row'>
+      <div class='col-lg-3 col-sm-6'>
+        <div class='card hovercard'>
+          <div class='cardheader'></div>
+          <div class='avatar'>
+            <img alt='' src={avatar} />
+          </div>
+          <div class='info'>
+            <div class='title'>
+              <a target='_blank'rel="noopener noreferrer" href='https://scripteden.com/'>
+                {name}
+              </a>
+            </div>
+            <div class='desc'>{status}</div>
+            <div class='desc'>{location && <span>{location}</span>}</div>
+          </div>
+          <div class='bottom'>
+            {social && social.facebook && (
+              <a
+                href={social.facebook}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+<i className="fab fa-facebook fa-2x"></i>                </a>
+            )}
+            {social && social.youtube && (
+              <a
+                href={social.youtube}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
         <i className="fab fa-youtube fa-2x"></i>
-                </a>
-        )
-      }
-         
-         {
-        social && social.instagram &&  (
-          <a href={social.instagram} target="_blank" rel="noopener noreferrer">
+              </a>
+            )}
+            {social && social.instagram && (
+              <a
+                href={social.instagram}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
         <i className="fab fa-instagram fa-2x"></i>
-                </a>
-        )
-      }
-      
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  )
-}
-
+);
+};
 ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired
-
-}
-
-export default ProfileTop
+profile: PropTypes.object.isRequired
+};
+export default ProfileTop;
