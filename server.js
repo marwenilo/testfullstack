ee28d35fec93b9ab14f1e52f5e70a9f4ff4f78a6
marwenilo <marwenmagrii@gmail.com>
2019-11-01 14:49:18 +0100

    heroku

--- server.js
+++ server.js
@@ -30,7 +30,11 @@ app.use(express.json({ extended: false}));
 
 // Passport middleware
 
-
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 // Passport Config
 // require('./config/passport')(passport);
 
