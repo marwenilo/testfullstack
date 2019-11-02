239143f73579616cfd8f6b409ab19a33ccb25bfc
marwenilo <marwenmagrii@gmail.com>
2019-11-02 11:35:44 +0100

    heroku2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require ('path'); 
 // const bodyParser = require('body-parser');
 
 
@@ -32,9 +33,7 @@ app.use(express.json({ extended: false}));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+
 // Passport Config
 // require('./config/passport')(passport);
 
@@ -43,5 +42,16 @@ app.use('/api/users', require('./routes/api/users') );
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+
+//server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
