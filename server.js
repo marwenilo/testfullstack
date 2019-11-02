2d5d8767b6034e47acc95e8ea6ba7c0ab2d20d82
marwenilo <marwenmagrii@gmail.com>
2019-11-02 12:02:32 +0100

    remove heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require ('path'); 
+// const path = require ('path'); 
 // const bodyParser = require('body-parser');
 
 
@@ -45,13 +45,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 
 //server static assets in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'));
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
